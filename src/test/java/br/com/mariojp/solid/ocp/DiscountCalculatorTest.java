e9cc52610dd852dcd4b26ee2923697297a2468a6
Davi Goes <davi.vasconcelos@ucsal.edu.br>
2025-09-02 00:14:56 -0300

    Open/Closed Principle (OCP)

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -1,13 +1,19 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DiscountCalculatorTest {
     @Test
-    void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
-        assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
-            "PARTNER deveria ter 12% de desconto");
+    void premium_gets_10_percent(){
+        Map<CustomerType, DiscountPolicy> policies = new HashMap<>();
+        policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        DiscountCalculator calc = new DiscountCalculator(policies);
+
+        assertEquals(90.0, calc.apply(100.0, CustomerType.PREMIUM), 0.0001);
     }
-}
+    }
+
